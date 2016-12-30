d15a53d29da059339b023b293fe413bf8b4d02f9
Federick Kwok <fedomain@gmail.com>
2016-12-30 15:21:21 +1100

    Making the grid and the control pad

--- js/cell.js
+++ js/cell.js
@@ -1,14 +1,29 @@
-function Cell(row, col) {
+function Cell(row, col, gridSize, cellCount) {
 	this.row = row;
 	this.col = col;
 	this.name = 'R' + this.row + 'C' + this.col;
 	this.id = 'r' + this.row + 'c' + this.col;
+	this.styleClass = '';
+	this.gridSize = gridSize;
+	this.cellCount = cellCount;
+
+	this.init = function() {
+		this.styleClass = this.getStyleClass();
+	}
 
 	this.getStyleClass = function() {
-		if (this.row == ) {
-			return 'highlighted';
+		if (this.col == this.gridSize && this.row == this.gridSize) {
+			return '';
+
+		} else if (this.col == this.gridSize) {
+			return'row-last';
+
+		} else if (this.row == this.gridSize) {
+			return 'col-last';
 		}
 
 		return 'normal';
 	}
+
+	this.init();
 }
--- js/game.js
+++ js/game.js
@@ -3,23 +3,28 @@ function Game(variation, difficulty, name) {
 	this.variation = variation;
 	this.difficulty = difficulty;
 	this.gridSize = 12; // Hardcoding this for now
+	this.gameArray = [];
 
 	this.startTimer = function() {
-		this.value = value;
+		return true;
 	};
 
 	this.stopTimer = function() {
-		return this.value;
+		return true;
 	};
 
 	this.setup = function() {
-		var gameGrid = '<table border=1 class="game-board">';
+		var count = 1;
+		var gameGrid = '<table class="game-board">';
 
 		for (var i=1; i <= this.gridSize; i++) {
 			gameGrid += '<tr>';
+			gameArray[i] = [];
 
 			for (var j=1; j <= this.gridSize; j++) {
-				gameGrid += '<td id="' + id + '">' + id + '</td>';
+				gameArray[i][j] = new Cell(i,j, this.gridSize, count);
+				gameGrid += '<td id="' + gameArray[i][j].id + '" class="' + gameArray[i][j].styleClass + '">' + gameArray[i][j].name + ' (' + count + ')' + '</td>';
+				count++;
 			}
 
 			gameGrid += '</tr>';
